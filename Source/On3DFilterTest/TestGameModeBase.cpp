93ef258139bcfc58fd708acdff44949d944b3399
José Luis <alagunasalahaddin@live.com>
2020-03-19 18:39:52 +0300

    :tada: Sending victory message to the user.

--- Source/On3DFilterTest/TestGameModeBase.cpp
+++ Source/On3DFilterTest/TestGameModeBase.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "TestGameModeBase.h"
 #include "On3DCharacter.h"
+#include "Containers/UnrealString.h"
+#include "Kismet/GameplayStatics.h"
 #include "UObject/ConstructorHelpers.h"
 
 ATestGameModeBase::ATestGameModeBase()
@@ -18,6 +20,13 @@ void ATestGameModeBase::OnCharacterDead(AActor* Actor){
 	bool bIsRemoved = EnemyArray.Remove(Actor);
 	bool bIsWin = EnemyArray.Num() <= 0 ? true : false;
 
-	if(bIsRemoved && bIsWin)
-	UE_LOG(LogTemp, Warning, TEXT("Winner!"), );
+	if(bIsRemoved && bIsWin){
+		GameOver(FName(TEXT("Winner!")));
+	UE_LOG(LogTemp, Warning, TEXT("Winner!"), )
+	}
+	else{
+		GameOver(FName(TEXT("Loser!")));
+	UE_LOG(LogTemp, Warning, TEXT("Loser!"), );
+	}
+
 }
